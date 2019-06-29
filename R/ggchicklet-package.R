26290939dc129124ec25fc53d38788db416d9273
boB Rudis <bob@rud.is>
2019-06-28 20:52:24 -0400

    R package repo initialization complete

---
+++ R/ggchicklet-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/ggchicklet>
+#' - BugReports: <https://gitlab.com/hrbrmstr/ggchicklet/issues>
+#' 
+#' @md
+#' @name ggchicklet
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
---
+++ tests/testthat/test-ggchicklet.R
@@ -0,0 +1,6 @@
+context("minimal package functionality")
+test_that("we can do something", {
+
+  #expect_that(some_function(), is_a("data.frame"))
+
+})
