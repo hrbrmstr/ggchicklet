1cd3a8e76d1292c9c7b282063441d71575705d5f
hrbrmstr <bob@rud.is>
2019-06-30 12:41:53 -0400

    tests

--- tests/testthat/test-ggchicklet.R
+++ tests/testthat/test-ggchicklet.R
@@ -1,6 +1,21 @@
-context("minimal package functionality")
-test_that("we can do something", {
+library(ggplot2)
 
-  #expect_that(some_function(), is_a("data.frame"))
+context("chicklet geoms are created properly")
+test_that("chicklet geoms are created properly", {
+
+  data("debates2019")
+
+  spkr_ordr <- aggregate(elapsed ~ speaker, data = debates2019, sum)
+  spkr_ordr <- spkr_ordr[order(spkr_ordr[["elapsed"]]),]
+
+  debates2019$speaker <- factor(debates2019$speaker, spkr_ordr$speaker)
+
+  ggplot(debates2019) +
+  geom_chicklet(aes(speaker, elapsed, group = timestamp, fill = topic)) -> gg
+
+  ggplot_build(gg) -> gb
+
+  expect_true(all(c("GeomChicklet", "GeomRrect") %in% class(gb$plot$layers[[1]]$geom)))
+
 
 })
