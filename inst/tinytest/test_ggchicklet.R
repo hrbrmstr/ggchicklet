822c3de6a52d51501309baca44395ba9b135dbd2
hrbrmstr <bob@rud.is>
2019-07-04 12:22:46 -0400

    switch to tinytest

---
+++ inst/tinytest/test_ggchicklet.R
@@ -0,0 +1,20 @@
+library(ggplot2)
+
+data("debates2019")
+
+spkr_ordr <- aggregate(elapsed ~ speaker, data = debates2019, sum)
+spkr_ordr <- spkr_ordr[order(spkr_ordr[["elapsed"]]),]
+
+debates2019$speaker <- factor(debates2019$speaker, spkr_ordr$speaker)
+
+ggplot(debates2019) +
+  geom_chicklet(aes(speaker, elapsed, group = timestamp, fill = topic)) -> gg
+
+print(gg)
+
+ggplot_build(gg) -> gb
+
+ggplot_gtable(gb) -> gt
+
+expect_true(all(c("GeomChicklet", "GeomRrect") %in% class(gb$plot$layers[[1]]$geom)))
+
